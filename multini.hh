345a567f75d61f6f22686724fc5f16ecd0f8c99a
bramtechs <bramtech@telenet.be>
2024-12-20 16:43:19 +0100

    work in progress

---
+++ multini.hh
@@ -0,0 +1,46 @@
+﻿#pragma once
+#include <map>
+#include <ranges>
+#include <string>
+#include <string_view>
+
+namespace multini {
+
+/**
+ * @brief INI reader that supports non-unique section names.
+ * Uses std::multimap under the hood.
+ */
+class INIReader {
+public:
+    INIReader(const std::string_view& sv)
+    {
+    }
+
+private:
+    static auto splitLines(const std::string_view& sv)
+    {
+        return sv
+            | std::views::split('\n')
+            | std::views::transform([](const auto& line) {
+                  auto trimmedLine = std::string_view(line.begin(), line.end());
+                  if (!trimmedLine.empty() && trimmedLine.back() == '\r') {
+                      trimmedLine.remove_suffix(1);
+                  }
+                  return trimmedLine;
+              });
+    }
+
+    static auto filterLines(const std::string_view& sv)
+    {
+        return splitLines(sv)
+            | std::views::filter([](const auto& line) {
+                  return !line.empty();
+              })
+            | std::views::filter([](const auto& line) {
+                  return line.starts_with(";");
+              });
+    }
+
+    std::multimap<std::string, std::map<std::string, std::string>> mSections;
+};
+}
---
+++ multini_tests.cc
@@ -0,0 +1,77 @@
+﻿#include <gtest/gtest.h>
+#include <iostream>
+#include <string_view>
+
+#define private public
+#include "multini.hh"
+
+static constexpr std::string_view trimmed(std::string_view&& str)
+{
+    while (!str.empty() && std::isspace(str.front()))
+        str.remove_prefix(1);
+
+    while (!str.empty() && std::isspace(str.back()))
+        str.remove_suffix(1);
+
+    return str;
+}
+
+TEST(multini, SplitLinesOfConfig)
+{
+    const std::string_view config = trimmed(R"(
+[section1]
+key1=value1
+key2=value2
+
+[section2]
+key3=value3
+)");
+
+    auto view = multini::INIReader::splitLines(config);
+    auto lines = std::vector<std::string_view>(view.begin(), view.end());
+
+    ASSERT_EQ(lines.size(), 6);
+    ASSERT_EQ(lines[0], "[section1]");
+    ASSERT_EQ(lines[1], "key1=value1");
+    ASSERT_EQ(lines[2], "key2=value2");
+    ASSERT_EQ(lines[3], "");
+    ASSERT_EQ(lines[4], "[section2]");
+    ASSERT_EQ(lines[5], "key3=value3");
+}
+
+TEST(multini, LinesWithCommentsOrEmptyShouldBeExcluded)
+{
+    const std::string_view config = trimmed(R"(
+; whatever
+; blabla
+; some text
+;
+;; test123
+[section1]
+key1=value1
+key2=value2
+
+;; more rambling
+
+;; test
+)");
+
+    auto view = multini::INIReader::filterLines(config);
+    auto lines = std::vector<std::string_view>(view.begin(), view.end());
+
+    ASSERT_EQ(lines.size(), 3);
+    ASSERT_EQ(lines[0], "[section1]");
+    ASSERT_EQ(lines[1], "key1=value1");
+    ASSERT_EQ(lines[2], "key2=value2");
+}
+
+TEST(multini, ConfigShouldHaveSection)
+{
+    const std::string_view config = trimmed(R"(
+; whatever
+[section1]
+key1=value1
+)");
+
+    multini::INIReader reader(config);
+}
